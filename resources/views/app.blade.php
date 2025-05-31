81a6d293e5f55eb5244e293e7a8ba79bb3bf0bc9
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-05-31 19:26:41 +0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
