78a810c1d201014ebe05f40145f8dee8713f8e2c
severusDude <fajarsaladin05@gmail.com>
2025-06-01 12:22:02 +0700

    feat (roles): add seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('admin')
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@example.com',
+                'password' => Hash::make('user')
+            ]
+        ];
+
+        foreach ($users as $user) {
+            \App\Models\User::create($user);
+        }
+
+    }
+}
