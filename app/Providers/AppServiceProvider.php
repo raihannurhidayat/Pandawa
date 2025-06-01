5b7d0bd5a970d999707f2d1d792fcb99eb98b6f1
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2025-06-01 12:22:36 +0700

    Feature/user-roles (#1)
    * fix (routing): routing error when in dev environment
    
    * feat (roles): add user roles
    
    * feat (roles): add seeder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (app()->environment('production')) {
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
     }
