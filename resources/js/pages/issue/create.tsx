39bcd6793d17e8415975625394c3bfc712bea2a2
severusDude <fajarsaladin05@gmail.com>
2025-06-02 00:10:41 +0700

    feat (issues): add create to layout

--- resources/js/pages/issue/create.tsx
+++ resources/js/pages/issue/create.tsx
@@ -1,5 +1,5 @@
 import { FormEventHandler, useState } from "react";
-import { Head, useForm } from "@inertiajs/react";
+import { Head, Link, useForm, usePage } from "@inertiajs/react";
 import {
     Card,
     CardContent,
@@ -22,6 +22,7 @@ import {
 import { Button } from "@/components/ui/button";
 import LocationFilter from "@/components/location-filter";
 import { AttachmentInput } from "@/components/attachments-input";
+import AuthenticatedLayout from "@/layouts/authenticated-layout";
 
 function CreateIssue({ categories }: { categories: any[] }) {
     const { data, setData, post, processing, errors } = useForm({
@@ -43,77 +44,82 @@ function CreateIssue({ categories }: { categories: any[] }) {
     };
 
     return (
-        <div className="flex items-center justify-center w-screen h-screen">
+        <AuthenticatedLayout header="Pengaduan">
             <Head title="Pengaduan" />
 
-            <form onSubmit={submit}>
-                <Card className="flex flex-col max-w-sm gap-4 mx-auto">
-                    <CardHeader className="flex flex-col items-center justify-center gap-2">
-                        <CardTitle className="text-2xl">
-                            Ajukan Pengaduan
-                        </CardTitle>
-                        <CardDescription>Buat pengaduan baru</CardDescription>
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Select
-                                onValueChange={handleIssueCategoryChange}
-                                required
-                            >
-                                <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Pilih kategori" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectGroup>
-                                        {categories?.map((category) => (
-                                            <SelectItem
-                                                key={category.id}
-                                                value={category?.id}
-                                            >
-                                                {category?.name}
-                                            </SelectItem>
-                                        ))}
-                                    </SelectGroup>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="title">Judul Pengaduan</Label>
-                            <Input
-                                id="title"
-                                type="text"
-                                placeholder="Judul..."
-                                value={data.title}
-                                onChange={(e) =>
-                                    setData("title", e.target.value)
-                                }
-                                required
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="body">Deskripsi Pengaduan</Label>
-                            <Textarea
-                                id="body"
-                                rows={4}
-                                placeholder="Deskripsikan pengaduanmu..."
-                                value={data.body}
-                                onChange={(e) =>
-                                    setData("body", e.target.value)
-                                }
-                                required
-                                className="resize-none"
-                            />
-                        </div>
+            <div className="flex items-center justify-center w-full">
+                <form onSubmit={submit}>
+                    <Card className="flex flex-col max-w-sm gap-4 mx-auto">
+                        <CardHeader className="flex flex-col items-center justify-center gap-2">
+                            <CardTitle className="text-2xl">
+                                Ajukan Pengaduan
+                            </CardTitle>
+                            <CardDescription>
+                                Buat pengaduan baru
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Select
+                                    onValueChange={handleIssueCategoryChange}
+                                    required
+                                >
+                                    <SelectTrigger className="w-full">
+                                        <SelectValue placeholder="Pilih kategori" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            {categories?.map((category) => (
+                                                <SelectItem
+                                                    key={category.id}
+                                                    value={category?.id}
+                                                >
+                                                    {category?.name}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="title">Judul Pengaduan</Label>
+                                <Input
+                                    id="title"
+                                    type="text"
+                                    placeholder="Judul..."
+                                    value={data.title}
+                                    onChange={(e) =>
+                                        setData("title", e.target.value)
+                                    }
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="body">
+                                    Deskripsi Pengaduan
+                                </Label>
+                                <Textarea
+                                    id="body"
+                                    rows={4}
+                                    placeholder="Deskripsikan pengaduanmu..."
+                                    value={data.body}
+                                    onChange={(e) =>
+                                        setData("body", e.target.value)
+                                    }
+                                    required
+                                    className="resize-none"
+                                />
+                            </div>
 
-                        <LocationFilter
-                            onChange={(loc: any) =>
-                                setData("location", JSON.stringify(loc))
-                            }
-                        />
+                            <LocationFilter
+                                onChange={(loc: any) =>
+                                    setData("location", JSON.stringify(loc))
+                                }
+                            />
 
-                        <div className="grid gap-2">
-                            <Label htmlFor="attachments">Attachments</Label>
-                            {/* <Input
+                            <div className="grid gap-2">
+                                <Label htmlFor="attachments">Attachments</Label>
+                                {/* <Input
                                 id="attachments"
                                 type="file"
                                 required
@@ -121,20 +127,21 @@ function CreateIssue({ categories }: { categories: any[] }) {
                                     setData("attachments", e?.target?.files[0])
                                 }
                             /> */}
-                            <AttachmentInput
-                                attachments={data.attachments}
-                                setAttachments={(files) =>
-                                    setData("attachments", files)
-                                }
-                            />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button type="submit">Ajukan</Button>
-                    </CardFooter>
-                </Card>
-            </form>
-        </div>
+                                <AttachmentInput
+                                    attachments={data.attachments}
+                                    setAttachments={(files) =>
+                                        setData("attachments", files)
+                                    }
+                                />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button type="submit">Ajukan</Button>
+                        </CardFooter>
+                    </Card>
+                </form>
+            </div>
+        </AuthenticatedLayout>
     );
 }
 
