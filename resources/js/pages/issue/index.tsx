597901a7d2a41b0e6b9a818252f99b1b01aeb904
severusDude <fajarsaladin05@gmail.com>
2025-06-01 12:34:38 +0700

    feat (issues): add issues index

---
+++ resources/js/pages/issue/index.tsx
@@ -0,0 +1,7 @@
+function Issue() {
+    return ( <>
+    <h1>Issue</h1>
+    </> );
+}
+
+export default Issue;
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
@@ -13,6 +14,8 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::resource('/pengaduan', IssueController::class)->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('dashboard');
