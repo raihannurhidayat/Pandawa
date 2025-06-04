e6cf7aca0c1ba686403460a9abe0c9755845171a
severusDude <fajarsaladin05@gmail.com>
2025-06-05 01:09:07 +0700

    style (components): update attachments input

--- resources/js/components/phase-create.tsx
+++ resources/js/components/phase-create.tsx
@@ -10,9 +10,14 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { AttachmentsInput } from "@/components/attachments-input";
+import { useState } from "react";
 
 function PhaseCreate({ children }: { children: React.ReactNode }) {
+    const [attachments, setAttachments] = useState<File[]>([]);
+
     return (
         <Dialog>
             <form>
@@ -24,9 +29,15 @@ function PhaseCreate({ children }: { children: React.ReactNode }) {
                             Add updates to this phase
                         </DialogDescription>
                     </DialogHeader>
-                    <div>
-                        <Label htmlFor="title">Set Title</Label>
-                        <Input id="title" type="text" />
+                    <div className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="reason">Set Reason</Label>
+                            <Textarea id="reason" className="resize-none" />
+                        </div>
+                        <AttachmentsInput
+                            attachments={attachments}
+                            setAttachments={setAttachments}
+                        />
                     </div>
                     <DialogFooter>
                         <DialogClose asChild>
