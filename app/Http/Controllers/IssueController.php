b0f91a58ad2b7789956079e617a8a8f200c7fa1e
severusDude <fajarsaladin05@gmail.com>
2025-06-10 10:53:14 +0700

    feat (phases): update phase with template based on received status

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Enums\PhaseStatus;
+use App\IssueProgressTemplates;
 use App\Models\Issue;
 use App\Models\IssueCategory;
 use App\Models\Phase;
@@ -112,12 +113,24 @@ class IssueController extends Controller
 
         $phase = Phase::findOrFail($id);
 
+        $status = $validated['status'];
+        $order = $phase->order;
+
+        // Retrieve template based on phase order and status
+        $template = IssueProgressTemplates::ISSUE_PROGRESS_TEMPLATES[$order][$status] ?? null;
+
         if ($request->hasFile('attachments')) {
             foreach ($request->file('attachments') as $file) {
                 $phase->addAttachment($file);
             }
         }
 
-        $phase->update($validated);
+        // Update the phase with template values
+        $phase->update([
+            'title' => $template['title'],
+            'body' => $template['body'],
+            'status' => $template['status'],
+            'reason' => $validated['reason'] ?? null,
+        ]);
     }
 }
