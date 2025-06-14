3576d9c70f93209b6166940ba38352c55d60e5bf
severusDude <fajarsaladin05@gmail.com>
2025-06-14 21:12:27 +0700

    feat (components): add icon to user avatar indicator

--- resources/js/components/user-avatar.tsx
+++ resources/js/components/user-avatar.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 import { PageProps, User } from "@/types";
 import { usePage } from "@inertiajs/react";
 import { cva, VariantProps } from "class-variance-authority";
-import { User as UserIcon } from "lucide-react";
+import { Edit, User as UserIcon } from "lucide-react";
 
 const userAvatarVariants = cva("size-10", {
     variants: {
@@ -12,9 +12,17 @@ const userAvatarVariants = cva("size-10", {
             md: "size-10",
             lg: "size-20",
         },
+        indicator: {
+            default: "bg-primary",
+            secondary: "bg-secondary",
+            danger: "bg-yellow-500 dark:bg-yellow-600",
+            destructive: "bg-destructive",
+            disabled: "bg-none",
+        },
     },
     defaultVariants: {
         size: "md",
+        indicator: "disabled",
     },
 });
 
@@ -32,6 +40,8 @@ export interface UserAvatarProps
      * If true, renders a user info block from user info.
      */
     showInformation?: boolean;
+
+    icon?: React.ReactNode;
 }
 
 export default function UserAvatar({
@@ -39,6 +49,8 @@ export default function UserAvatar({
     size = "md",
     className = "",
     showInformation = false,
+    indicator = "disabled",
+    icon = null,
 }: UserAvatarProps) {
     return (
         <div
@@ -47,12 +59,31 @@ export default function UserAvatar({
                 className
             )}
         >
-            <Avatar className={cn(userAvatarVariants({ size }))}>
-                <AvatarImage src={user?.profile_photo_path} />
-                <AvatarFallback className="flex items-center justify-center w-full h-full">
-                    <UserIcon className="w-1/2 h-1/2" />
-                </AvatarFallback>
-            </Avatar>
+            <div className="relative overflow-visible">
+                <Avatar className={cn(userAvatarVariants({ size }))}>
+                    <AvatarImage src={user?.profile_photo_path} />
+                    <AvatarFallback className="flex items-center justify-center w-full h-full">
+                        <UserIcon className="w-1/2 h-1/2" />
+                    </AvatarFallback>
+                </Avatar>
+
+                {(indicator !== "disabled" || !!icon) && (
+                    <div
+                        className={cn(
+                            icon
+                                ? "bg-accent"
+                                : userAvatarVariants({ indicator }),
+                            "absolute rounded-full",
+                            "w-[clamp(0.75rem,35%,1.5rem)] h-[clamp(0.75rem,35%,1.5rem)]",
+                            "top-[calc(50%+35.36%)] left-[calc(50%+35.36%)]",
+                            "transform -translate-x-1/2 -translate-y-1/2"
+                            // tailwind black magic stuff , don't touch it
+                        )}
+                    >
+                        {icon}
+                    </div>
+                )}
+            </div>
 
             {showInformation && (
                 <div className="flex flex-col items-center gap-1 leading-tight lg:items-start">
